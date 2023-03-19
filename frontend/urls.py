160c97a97bf4f487f09ba9865c6c8de81f76c388
Kayat-Prem <imkayat7@gmail.comm>
2023-03-19 13:34:07 +0545

    added full working forgot password page

--- frontend/urls.py
+++ frontend/urls.py
@@ -7,5 +7,7 @@ path("",views.index),
 path("login/",views.login_view),
 path('register/',views.register_user),
 path("contact/",views.contact),
-path("forgot-password/",views.forgot_password)
+path("forgot-password/",views.forgot_password),
+path("forgot-password2/",views.forgot_password2),
+path("forgot-password3/",views.forgot_password3)
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,6 +3,7 @@ from . import forms
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from .models import School
+from django.contrib.auth.models import User
 # Create your views here.
 
 def index(request):
@@ -24,7 +25,7 @@ def login_view(request):
                 if School.objects.filter(user=user).exists():
                   return redirect('/shool-dashboard') # Redirect to home page
                 else:
-                    return redirect("/admin-dashboard")
+                    return redirect("/admin-dashboards")
                 
             else:
                 msg = 'Invalid credentials'
@@ -32,7 +33,6 @@ def login_view(request):
             msg = 'Error validating the form'
 
     return render(request, "frontend/home/login.html", {"form": form, "msg": msg})
-
 
 def register_user(request):
     msg = None
@@ -46,7 +46,9 @@ def register_user(request):
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
             status = "new" # set default status
-            School.objects.create(user=user, status=status)
+            answer = request.POST.get('security_question')
+            School.objects.create(user=user, status=status,security_question="first word of the school if you want to rename your school",security_answer=answer)
+
             msg = 'User created - please <a href="/login">login</a>.'
             success = True
 
@@ -64,4 +66,33 @@ def contact(request):
 
 
 def forgot_password(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        print("username")
+        return redirect(f"/forgot-password2?key={username}")
+
     return render(request, "frontend/home/forgot_password.html")
+
+def forgot_password2(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        answer=request.POST.get("answer")
+        return redirect(f"/forgot-password3?key={username}")
+    return render(request, "frontend/home/forgot_password2.html")
+
+def forgot_password3(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get("password")
+        user = User.objects.get(username=username)
+
+    # Set the new password for the user
+
+        user.set_password(password)
+
+    # Save the user object to update the password in the database
+        user.save()
+        return redirect("/login")
+    
+
+    return render(request, "frontend/home/forgot_password3.html")
